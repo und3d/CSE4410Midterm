b9be798dc9dd1929241f5f2ac0a286f62affe3a3
John1010015 <johnchs218@gmail.com>
2024-03-16 09:51:42 -0700

    Added lose condition

--- FPS_3D/Assets/Scripts/PlayerCharacter.cs
+++ FPS_3D/Assets/Scripts/PlayerCharacter.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-
     int health;
     public UiController uiController;
 
     // Start is called before the first frame update
     void Start()
     {
-        health = 5;
+        health = 3;
         uiController.UpdateHealthText(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for the lose condition (health reaches zero)
+        if (health <= 0)
+        {
+            Debug.Log("You lose!");
+
+            gameObject.SetActive(false);
+        }
     }
 
     public void Hurt(int damage)

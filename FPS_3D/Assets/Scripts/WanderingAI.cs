f70ad7ac2186f8db5dd207307daa9b4a0b919aea
frewpy <frewp2013@gmail.com>
2024-03-19 10:43:50 -0700

    AI Chasing Behavior
    AI now has chasing behavior if in detection range, otherwise will return to default wandering state that was used previously

--- FPS_3D/Assets/Scripts/WanderingAI.cs
+++ FPS_3D/Assets/Scripts/WanderingAI.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class WanderingAI : MonoBehaviour
 {
     [SerializeField] GameObject fireballPrefab;
@@ -11,10 +13,13 @@ public class WanderingAI : MonoBehaviour
     public float obstacleRange = 5.0f;
 
     bool isAlive;
+    public float detectionRange = 10.0f;
+    private GameObject player;
 
     private void Start()
     {
         isAlive = true;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -22,34 +27,47 @@ public class WanderingAI : MonoBehaviour
     {
         if (isAlive)
         {
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        }
+            float distance = Vector3.Distance(player.transform.position, transform.position);
 
-        Ray ray = new Ray(transform.position, transform.forward);
-
-        RaycastHit hit;
-
-        if (Physics.SphereCast(ray, 0.75f, out hit))
-        {
-            GameObject hitObject = hit.transform.gameObject;
-
-            if (hitObject.GetComponent<PlayerCharacter>())
+            // chasing behavior towards player
+            if (distance < detectionRange)
             {
-                if (fireball == null)
-                {
-                    fireball = Instantiate(fireballPrefab) as GameObject;
-                    fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f) + new Vector3(0, 1, 0);
-                    fireball.transform.rotation = transform.rotation;
-                }
+                Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+                transform.Translate(directionToPlayer * speed * Time.deltaTime, Space.World);
+
+                // rotate towards the player, pretty smooth rotation
+                Quaternion lookRotation = Quaternion.LookRotation(new Vector3(directionToPlayer.x, 0, directionToPlayer.z));
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
             }
-
-            if (hit.distance < obstacleRange)
+            else
             {
-                float angle = Random.Range(-110, 110);
-                transform.Rotate(0, angle, 0);
+                // return to old wandering state 
+                transform.Translate(0, 0, speed * Time.deltaTime);
+
+                Ray ray = new Ray(transform.position, transform.forward);
+                RaycastHit hit;
+                if (Physics.SphereCast(ray, 0.75f, out hit, obstacleRange))
+                {
+                    GameObject hitObject = hit.transform.gameObject;
+                    if (hitObject.GetComponent<PlayerCharacter>())
+                    {
+                        if (fireball == null)
+                        {
+                            fireball = Instantiate(fireballPrefab) as GameObject;
+                            fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                            fireball.transform.rotation = transform.rotation;
+                        }
+                    }
+                    else if (hit.distance < obstacleRange)
+                    {
+                        float angle = Random.Range(-110, 110);
+                        transform.Rotate(0, angle, 0);
+                    }
+                }
             }
         }
     }
+
 
     public void SetAlive(bool alive)
     {

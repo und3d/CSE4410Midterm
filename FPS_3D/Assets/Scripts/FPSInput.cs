3b520f7ca41dc17b80ac48c82d9788c6c8a85009
und3d <cckokanour@gmail.com>
2024-03-20 15:31:58 -0700

    Actually merged Jump Branch

--- FPS_3D/Assets/Scripts/FPSInput.cs
+++ FPS_3D/Assets/Scripts/FPSInput.cs
@@ -4,33 +4,41 @@ using UnityEngine;
 
 public class FPSInput : MonoBehaviour
 {
-
     public float speed = 3.0f;
     public float gravity = -9.8f;
+    public float jumpSpeed = 7.5f;
     private CharacterController charController;
+    private float verticalSpeed = 0; // track vertical speed separately
 
-    // Start is called before the first frame update
     void Start()
     {
         charController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
-
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-
         movement = Vector3.ClampMagnitude(movement, speed);
 
-        movement.y = gravity;
+        // checks if player os on the ground
+        if (charController.isGrounded)
+        {
+            verticalSpeed = gravity * Time.deltaTime;
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalSpeed = jumpSpeed; // apply jump force
+            }
+        }
+        else
+        {
+            verticalSpeed += gravity * Time.deltaTime; // apply gravity while in air
+        }
 
+        movement.y = verticalSpeed;
         movement *= Time.deltaTime;
-
         movement = transform.TransformDirection(movement);
-
         charController.Move(movement);
     }
 }
